c9bb1173e4e9a47c3c97301b76fbec96474270cc
Asim M <asim@Winterfell.local>
2016-11-01 00:44:49 -0400

    modifying server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
+    res.end('Four things support the world:the learning of the wise, the justice of the great, the prayers of the good, and the valor of the brave');
+
 }).listen(process.env.PORT || 8080);
